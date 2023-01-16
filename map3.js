1d31616027f4587d4177e3a6ed1d9540869f7910
TestSAP <116174511+TestSAP@users.noreply.github.com>
2023-01-16 17:11:06 +0400

    Update map3.js

--- map3.js
+++ map3.js
@@ -77,10 +77,9 @@
 
         view.when(function() {
           // when the scene and view resolve, display the scene's title in the DOM
-          //const title = gTitleID;
-            const gTitleID = scene.portalItem.gTitleID;
+          const title = gTitleID;
           //const title = scene.portalItem.title;
-          titleDiv.innerHTML = gTitleID;
+          titleDiv.innerHTML = title;
         });
       });
     }
