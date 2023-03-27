c2d4df46520532af8d749578a67b41b37c0ee45d
TestSAP <116174511+TestSAP@users.noreply.github.com>
2023-03-27 16:05:03 +0400

    Update test3DBuilder.js

--- test3DBuilder.js
+++ test3DBuilder.js
@@ -58,10 +58,6 @@
               <td><label for="layerOption">Layer Option:</label></td>
               <td><input id="layerOption" name="layerOption" type="text"></td>
             </tr>
-            <tr>
-              <td><label for="layerOption">Layer Option:</label></td>
-              <td><input id="layerOption" name="layerOption" type="text"></td>
-            </tr>
           </table>
         </fieldset>
         <button type="submit" hidden>Submit</button>
