553e910a4ee5aeb466c8105233f3772d885a0a8d
TestSAP <116174511+TestSAP@users.noreply.github.com>
2023-11-29 13:55:30 +0400

    Update compass_intern.js

--- compass_intern.js
+++ compass_intern.js
@@ -85,20 +85,7 @@
         (Map, MapView, Compass, FeatureLayer) => {
 
           mapValue = 1;
-        const url = "https://services1.arcgis.com/4yjifSiIG17X0gW4/arcgis/rest/services/FatalAccidents2017/FeatureServer";
 
-        const layer = new FeatureLayer({
-          title: "Fatal car accidents (2017)",
-          url
-        });
-        const map = new WebMap({
-          basemap: {
-            portalItem: {
-              id: "466f3f43c231453c938c6776777b89e2"
-            }
-          },
-          layers: [ layer ]
-        });
 
 
           gLayerURL.forEach(i => {
