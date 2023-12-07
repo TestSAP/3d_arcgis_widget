d8ca18af4d608f88298ab849631f741c2c799ca0
TestSAP <116174511+TestSAP@users.noreply.github.com>
2023-12-07 14:10:29 +0400

    Update compass_widget_intern_beacons.js

--- compass_widget_intern_beacons.js
+++ compass_widget_intern_beacons.js
@@ -1,14 +1,16 @@
-(function() {
-  let template = document.createElement('template');
+(function () {
+  let template = document.createElement("template");
+  var gLayerURL;
+  var gdegrees;
+  var gcenter;
+  var gzoom;
   var locationData; // holds up each beacons data
-  var webscene;
   var geojsonlayer;
-  var viewLayer;
   var url;
   var blob;
+  var map;
   var templates;
   var renderer;
-  var legend;
   var iniValue = 0;
   var pointArrFeatureCollection;
   var gPortalID;
@@ -16,122 +18,118 @@
   var gBOColor;
   var gBstartSize;
   var gBStopSize;
-  var glegendOption = "ON";
   var mapValue = 0;
-
-
+  var sum = 0;
+  var gchartMeasure;
 
   template.innerHTML = `
-        <link rel="stylesheet" href="https://js.arcgis.com/4.23/esri/themes/light/main.css" />
-    <script src="https://js.arcgis.com/4.23/"></script>
+    <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
+    <title>Compass widget | Sample | ArcGIS Maps SDK for JavaScript 4.27</title>
+
     <style>
-    html,
-    body,
-    #viewDiv {
-      padding: 0;
-      margin: 0;
-      height: 100%;
-      width: 100%;
-    }
-  </style>
-     <body>
-  <div id="viewDiv"></div>
-</body>
-</html>
+      html,
+      body,
+      #viewDiv {
+        padding: 0;
+        margin: 0;
+        height: 100%;
+        width: 100%;
+      }
+    </style>
+
+    <link rel="stylesheet" href="https://js.arcgis.com/4.27/esri/themes/light/main.css" />
+    <script src="https://js.arcgis.com/4.27/"></script>
+
+
+  </head>
+  <body>
+    <div id="viewDiv"></div>
+  </body>
+
     `;
-
-
-  //  fetch(https://ey-global-services-25.eu10.hcs.cloud.sap/sap/fpa/ui/app.html#/analyticapp&/aa/BA45D0589FB29287ABF8A582C05D41D/?mode=edit)
-
-
 
   // Convert string coordinate from geojson file to array of cooor
   function removeString(stringCoor) {
-    var LatLng = stringCoor.replace('[', '').replace(']', '').split(',')
-    var Lat = parseFloat(LatLng[0]);
-    var Lng = parseFloat(LatLng[1]);
-    return [Lng, Lat]
-  }
+    try
+    {
+     var LatLng = stringCoor.replace('[', '').replace(']', '').split(',')
+     var Lat = parseFloat(LatLng[0]);
+     var Lng = parseFloat(LatLng[1]);
+     return [Lng, Lat]
+    }
+    catch(e){
+     console.log("not a coordinate");
+    }
+   }
 
   // function to convert array to geojson format
   function j2gConvert(jsonObject) {
     const geoJSONPointArr = jsonObject.map((row) => {
+      sum = jsonObject[jsonObject.length - 1].sum;
+      console.log(row[gchartMeasure]);
       return {
         type: 'Feature',
         geometry: {
-          type: 'Point',
+          type: row.Geometry_Type,
           coordinates: removeString(row.Geometry_coordinates)
         },
         properties: {
-          beaconId: row.Properties_name_1,
-          aisle_name: row.Properties_Add_details,
+          beaconId: row.beaconID,
+          aisle_name: row.beaconName,
+          measure: std(row[gchartMeasure]),
         },
-        id: parseFloat(row.Properties_name_1),
+        id: parseFloat(row.beaconID),
       };
     });
 
     return geoJSONPointArr;
   }
+  function std(x){
 
+    var y = parseFloat(x)/sum*10;
+    console.log(sum);
+    console.log(parseFloat(y));
+    return parseFloat(y);
+
+  }
   function mainMap() {
-    require(
-      [
-        'esri/config',
-        'esri/Map',
-        'esri/views/SceneView',
-        'esri/WebScene',
-        'esri/Basemap',
-        'esri/layers/FeatureLayer',
-        'esri/widgets/LayerList',
-        'esri/request',
-        'dojo/domReady!',
-        'esri/layers/GraphicsLayer',
-        'esri/Graphic',
-        'esri/widgets/Legend',
-        'esri/layers/GeoJSONLayer',
-        'esri/tasks/RouteTask',
-        'esri/tasks/support/RouteParameters',
-        'esri/tasks/support/FeatureSet',
-      ],
-      (esriConfig, Map, SceneView, WebScene, Basemap, TileLayer, FeatureLayer,
-        LayerList, request, GraphicsLayer, Graphic, Legend, GeoJSONLayer,
-        RouteTask, RouteParameters, FeatureSet) => {
+    require(["esri/Map", "esri/views/MapView", "esri/widgets/Compass", "esri/layers/FeatureLayer"],
+      (Map, MapView, Compass, FeatureLayer) => {
 
         mapValue = 1;
 
-        // create the main map of type webscene
-        webscene = new WebScene({
-          portalItem: {
-            id: gPortalID,
-          },
+        map = new Map({
+          basemap: "streets-vector"
         });
 
-
-           // create a layer to hold the heatmap data
-           //var heatmapLayer = new GeoJSONLayer({
-            //url: '<https://ey-global-services-25.eu10.hcs.cloud.sap/sap/fpa/ui/app.html#/analyticapp&/aa/BA45D0589FB29287ABF8A582C05D41D/?mode=edit>',
-          //})
-          
-        
-
-     
-       // add the WebScene to the SceneView layer(Layer displayed)
-        viewLayer = new SceneView({
-          container: 'viewDiv',
-          map: webscene,
-        });
-
-        if (glegendOption == "on") {
-          // display a key on the screen containing all shapes in map
-          legend = new Legend({
-            view: viewLayer,
+        gLayerURL.forEach(i => {
+          const featureLayer = new FeatureLayer({
+            url: i
           });
+          map.add(featureLayer);
+        });
 
-          // add the key to the main screen
-          viewLayer.ui.add(legend, 'top-right');
+        const view = new MapView({
+          container: "viewDiv",
+          scale: 500000,
+          map: map,
+          zoom: 2,
+          center: [-138, 30],
+        });
 
-        }
+        /********************************
+         * Create a compass widget object.
+         *********************************/
 
+        const compassWidget = new Compass({
+          view: view
+        });
+
+        // Add the Compass widget to the top left corner of the view
+        view.ui.add(compassWidget, "top-left");
+        view.rotation = gdegrees;
 
         // template to display additional details for the beacon when selected
         templates = {
@@ -141,98 +139,44 @@
 
         // information on how to display the beacons(point format)
         renderer = {
-          type: 'simple',
-          field: 'name',
-          symbol: {
-            type: 'simple-marker',
-            color: gBeaconColor,
-            outline: {
-              color: gBOColor,
-            },
-          },
-          visualVariables: [{
-            type: 'size',
-            field: 'name',
-            stops: [{
-                value: 4,
-                size: gBstartSize,
-              },
-              {
-                value: 8,
-                size: gBStopSize,
-              },
-            ],
-          }, ],
+          type: "heatmap",
+          field: "measure",
+          colorStops: [
+            { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+            { color: "#472b77", ratio: 0.083 },
+            { color: "#4e2d87", ratio: 0.166 },
+            { color: "#563098", ratio: 0.249 },
+            { color: "#5d32a8", ratio: 0.332 },
+            { color: "#6735be", ratio: 0.415 },
+            { color: "#7139d4", ratio: 0.498 },
+            { color: "#7b3ce9", ratio: 0.581 },
+            { color: "#853fff", ratio: 0.664 },
+            { color: "#a46fbf", ratio: 0.747 },
+            { color: "#c29f80", ratio: 0.83 },
+            { color: "#e0cf40", ratio: 0.913 },
+            { color: "#ffff00", ratio: 1 }
+          ],
+          maxDensity: 0.1,
+          minDensity: 0
+          //radius: 10
         };
-       
-      // template to display additional details for the beacon when selected
-      const template = {
-        title: "Beacon Detail",
-        content: "Beacon ID:{beaconId} \n Aisle assigned to:{aisle_name}"
-      };
-
-      const layer = new CSVLayer({
-        url: url,
-        popupTemplate: template,
-        renderer: renderer,
-        labelsVisible: true,
-        labelingInfo: [
-          {
-            symbol: {
-              type: "text", // autocasts as new TextSymbol()
-              color: "white",
-              font: {
-                family: "Noto Sans",
-                size: 8
-              },
-              haloColor: "#472b77",
-              haloSize: 0.75
-            },
-            labelPlacement: "center-center",
-          }
-        ]
       });
-
-
-
-
-
-
-      });
-
   }
 
   // function inside class to create geojson beacons
   function processbeacons() {
     require(
       [
-        'esri/config',
-        'esri/Map',
-        'esri/views/SceneView',
-        'esri/WebScene',
-        'esri/Basemap',
-        'esri/layers/FeatureLayer',
-        'esri/widgets/LayerList',
-        'esri/request',
-        'dojo/domReady!',
-        'esri/layers/GraphicsLayer',
-        'esri/Graphic',
-        'esri/widgets/Legend',
         'esri/layers/GeoJSONLayer',
-        'esri/tasks/RouteTask',
-        'esri/tasks/support/RouteParameters',
-        'esri/tasks/support/FeatureSet',
       ],
-      (esriConfig, Map, SceneView, WebScene, Basemap, TileLayer, FeatureLayer,
-        LayerList, request, GraphicsLayer, Graphic, Legend, GeoJSONLayer,
-        RouteTask, RouteParameters, FeatureSet) => {
-
+      (GeoJSONLayer) => {
+        console.log(locationData)
         pointArrFeatureCollection = {};
         pointArrFeatureCollection = {
           type: 'FeatureCollection',
           features: j2gConvert(locationData),
           bbox: [
-            -179.9997, -61.6995, -3.5699999332428, 179.9142, 82.9995, 629.17
+            -179.9997, -61.6995, 179.9142, 82.9995 //2D bounding box
           ],
         };
 
@@ -250,157 +194,112 @@
           popupTemplate: templates,
           renderer: renderer
         });
-
-        var heatmapLayer = new GeoJSONLayer({
-          url,
-          popupTemplate: templates,
-          renderer: renderer
-        });
-
-        if (glayerOption == "on") {
-          // add the beacons to the webscene
-          webscene.add(geojsonlayer);
-          iniValue = 1;
-        }
-
+        map.add(geojsonlayer);
+        iniValue = 1;
+        console.log("layer loaded");
+        console.log(gchartMeasure);
+        console.log(pointArrFeatureCollection);
       });
   } // end of function bracket
 
   class Map extends HTMLElement {
     constructor() {
       super();
-      // this._shadowRoot = this.attachShadow({mode: "open"});
+
       this.appendChild(template.content.cloneNode(true));
       this._props = {};
-      let that = this;
-         getSelection() 
-        {
-         return this._currentSelection;
-       }
 
-
-      require(
-        [
-          'esri/Map', 'esri/views/SceneView', 'esri/WebScene', 'esri/Basemap',
-          'esri/layers/FeatureLayer', 'esri/widgets/LayerList', 'esri/request',
-          'dojo/domReady!', 'esri/layers/GraphicsLayer', 'esri/Graphic',
-          'esri/widgets/Legend', 'esri/layers/GeoJSONLayer'
-        ],
-        (Map, SceneView, WebScene, Basemap, TileLayer, FeatureLayer, LayerList,
-          request, GraphicsLayer, Graphic, Legend, GeoJSONLayer) => {
-
-
-        });
-    } // end of constructor()
+    } //end of constructor
 
     getSelection() {
       return this._currentSelection;
     }
-      
-    fetchDataFromChart() {
-      // Use the SAP Analytics Cloud API to fetch data from a chart
-      // Fetch data from a chart and assign it to locationData
-     locationData = fetchDataFromSACChart();
-   }
 
-    // function executed on initialisation
-    // function executed 2 times. first returns default value of variable &
-    // initialisation variables data
     onCustomWidgetBeforeUpdate(oChangedProperties) {
-      this.$chartData = oChangedProperties['chartData'];
-      locationData = this.$chartData;
-      if (locationData && !(gPortalID == null) && mapValue == 0)
+      locationData = oChangedProperties['chartData'];
+      if (locationData && !(gLayerURL == null) && mapValue == 0)
         mainMap();
     }
 
-        onCustomWidgetBeforeUpdate(oChangedProperties) {
-         // Fetch data from the chart before updating
-        this.fetchDataFromChart();
-    }
-
-    ////function executed on variable updates
     onCustomWidgetAfterUpdate(oChangedProperties) {
-      if ('legendOption' in oChangedProperties) {
-        this.$legendOption = oChangedProperties['legendOption'];
+      if('chartMeasure' in oChangedProperties){
+        gchartMeasure= oChangedProperties['chartMeasure'];
       }
-      glegendOption = this.$legendOption;
-      if ('layerOption' in oChangedProperties) {
-        this.$layerOption = oChangedProperties['layerOption'];
+      if ('layerURL' in oChangedProperties) {
+        gLayerURL = oChangedProperties['layerURL'].split(',').map(item => item.trim());
       }
-      glayerOption = this.$layerOption;
-      if ('portalId' in oChangedProperties) {
-        this.$portalId = oChangedProperties['portalId'];
+      if ('degrees' in oChangedProperties) {
+        gdegrees = oChangedProperties['degrees'];
       }
-      gPortalID = this.$portalId;
+      if ('center' in oChangedProperties) {
+        gcenter = JSON.parse(oChangedProperties['center']);
+      }
+      if ('zoom' in oChangedProperties) {
+        gzoom = oChangedProperties['zoom'];
+      }
       if ('BColor' in oChangedProperties) {
-        this.$BColor = oChangedProperties['BColor'];
+        gBeaconColor = oChangedProperties['BColor'];
       }
-      gBeaconColor = this.$BColor;
-      if ('BOColor' in oChangedProperties) {
-        this.$BOColor = oChangedProperties['BOColor'];
-      }
-      gBOColor = this.$BOColor;
-      if ('StartSize' in oChangedProperties) {
-        this.$StartSize = oChangedProperties['StartSize'];
-      }
-      gBstartSize = this.$StartSize;
-      if ('StopSize' in oChangedProperties) {
-        this.$StopSize = oChangedProperties['StopSize'];
-      }
-      gBStopSize = this.$StopSize;
 
-      if (!(gPortalID == null || gBeaconColor == null || gBOColor == null || gBstartSize == null || gBStopSize == null)) {
+      if ('BOColor' in oChangedProperties) {
+        gBOColor = oChangedProperties['BOColor'];
+      }
+
+      if ('StartSize' in oChangedProperties) {
+        gBstartSize = oChangedProperties['StartSize'];
+      }
+      if ('StopSize' in oChangedProperties) {
+        gBStopSize = oChangedProperties['StopSize'];
+      }
+      if (!(gLayerURL == null || gBeaconColor == null || gBOColor == null || gBstartSize == null || gBStopSize == null)) {
         if ('chartData' in oChangedProperties) {
-          this.$chartData = oChangedProperties['chartData'];
-          locationData = this.$chartData; // place passed in value into global
+          locationData = oChangedProperties['chartData'];
         }
         if (locationData) {
           if (iniValue == 1) { // remove previous geojsonlayer from webscene
-            webscene.remove(geojsonlayer);
+            map.remove(geojsonlayer);
           }
           processbeacons();
         }
       }
     }
 
+  } //end of class
 
-  } // end of class
-  let scriptSrc = 'https://js.arcgis.com/4.18/'
-  let onScriptLoaded =
-    function() {
-      customElements.define('com-sap-custom-jumbo-beacon-interface', Map);
-    }
+  let scriptSrc = "https://js.arcgis.com/4.18/"
+  let onScriptLoaded = function () {
+    customElements.define("com-sap-custom-geomap", Map);
+  }
 
-  // SHARED FUNCTION: reuse between widgets
-  // function(src, callback) {
-  let customElementScripts =
-    window.sessionStorage.getItem('customElementScripts') || [];
-  let scriptStatus = customElementScripts.find(function(element) {
+  //SHARED FUNCTION: reuse between widgets
+  //function(src, callback) {
+  let customElementScripts = window.sessionStorage.getItem("customElementScripts") || [];
+  let scriptStatus = customElementScripts.find(function (element) {
     return element.src == scriptSrc;
   });
 
   if (scriptStatus) {
-    if (scriptStatus.status == 'ready') {
+    if (scriptStatus.status == "ready") {
       onScriptLoaded();
     } else {
       scriptStatus.callbacks.push(onScriptLoaded);
     }
   } else {
     let scriptObject = {
-      'src': scriptSrc,
-      'status': 'loading',
-      'callbacks': [onScriptLoaded]
+      "src": scriptSrc,
+      "status": "loading",
+      "callbacks": [onScriptLoaded]
     }
     customElementScripts.push(scriptObject);
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
+    var script = document.createElement("script");
+    script.type = "text/javascript";
     script.src = scriptSrc;
-    script.onload = function() {
-      scriptObject.status = 'ready';
+    script.onload = function () {
+      scriptObject.status = "ready";
       scriptObject.callbacks.forEach((callbackFn) => callbackFn.call());
     };
     document.head.appendChild(script);
   }
 
-  // END SHARED FUNCTION
-})(); // end of class
+  //END SHARED FUNCTION
+})();
